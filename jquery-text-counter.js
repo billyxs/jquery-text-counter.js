a245afe575e8d768b3c09e0bcf5145035ded7ab3
billyxs <billymont@gmail.com>
2014-03-11 09:43:21 -0700

    add parent, suffix, prefix, and container params to override the limit element

--- jquery-text-counter.js
+++ jquery-text-counter.js
@@ -5,10 +5,18 @@
 (function($){
     $.fn.textcounter = function(options){
         var opts = $.extend({}, $.fn.textcounter.defaults, options);
+        var $limitElem = (opts.container !== null) ? opts.container : $('<span></span>');
+
+        $limitElem.addClass(opts.class);
+
+        console.log("container" , $limitElem);
 
         return this.each(function(){
             var $this = $(this);
-            $('<span class="textarea-limit-label"> (160)</span>').insertAfter( $this.parent().children('label') );
+
+            console.log("list elem", $limitElem);
+            if(opts.container === null)
+                $limitElem.insertBefore( $this );
 
             $this.each(updateTextareaCount);
 
@@ -16,10 +24,17 @@
         });
 
         function updateTextareaCount(){
+            console.log("limit elem", $limitElem);
+
             if( $(this).val().length>opts.limit )
                 $(this).val( $(this).val().substr(0, opts.limit) );
 
-            $(this).parent().children('.textarea-limit-label').html(' ('+(opts.limit-$(this).val().length)+')');
+            var count = (opts.limit-$(this).val().length);
+            $limitElem.html( buildCounterString(count) );
+        }
+
+        function buildCounterString(count) {
+            return opts.prefix + count + opts.suffix;
         }
 
     };
@@ -27,7 +42,10 @@
     //
     // plugin defaults
     //
-    $.fn.textcounter.defaults = {
-        limit: 160
-    };
+    $.fn.textcounter.defaults = { limit: 160
+                                , class: "textarea-limit-label"
+                                , container: null
+                                , prefix: ""
+                                , suffix: ""
+                                };
 })(jQuery);
