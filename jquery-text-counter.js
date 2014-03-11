b33315c1aaa38afbec2680f2fca0f5f43286d4f6
billyxs <billymont@gmail.com>
2014-03-10 20:56:29 -0700

    add initial demo files

---
+++ jquery-text-counter.js
@@ -0,0 +1,29 @@
+(function($){
+    $.fn.textcounter = function(options){
+        var opts = $.extend({}, $.fn.textcounter.defaults, options);
+
+        return this.each(function(){
+            var $this = $(this);
+            $('<span class="textarea_limit_label"> (160)</span>').insertAfter( $this.parent().children('label') );
+
+            $this.each(updateTextareaCount);
+
+            $this.keyup(updateTextareaCount);
+        });
+
+        function updateTextareaCount(){
+            if( $(this).val().length>opts.limit )
+                $(this).val( $(this).val().substr(0, opts.limit) );
+
+            $(this).parent().children('.textarea_limit_label').html(' ('+(opts.limit-$(this).val().length)+')');
+        }
+
+    };
+
+    //
+    // plugin defaults
+    //
+    $.fn.textcounter.defaults = {
+        limit: 160
+    };
+})(jQuery);
